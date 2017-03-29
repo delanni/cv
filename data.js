a94e28d87125067a637f4c7fc0779c88cc1fa946
Alex Szabo <delanni.alex@gmail.com>
2017-03-30 01:37:16 +0200

    improve with click and detailed description

---
+++ data.js
@@ -0,0 +1,129 @@
+[{
+    "category": "Budapest University of Technology",
+    "name": "Software Engineering, BSc",
+    "from": "2009-08-31",
+    "to": "2013-02-03",
+    "id": 0,
+    "position": "Software Engineering",
+    "description": "I did an engineering-infused computer science programme at Budapest's best technical university.",
+    "extras": {
+        "thesis": {
+            "title": "Windows 8 application development",
+            "description": "I developed a GPS based note-taking application for the newly opened Windows 8 modern platform.",
+            "technologies": ["C#", "MS Azure"]
+        }
+    }
+}, {
+    "category": "Budapest University of Technology",
+    "name": "Software Engineering, MSc",
+    "from": "2013-02-13",
+    "to": "2015-02-03",
+    "id": 1,
+    "position": "Software Engineering",
+    "description": "I followed up with a Master's in the same theme, putting focus on higher level computer software knowledge and spectrum widening courses.",
+    "extras": {
+        "thesis": {
+            "title": "Game development in Typescript using WebGL",
+            "description": "While exploring typescript and WebGL, I developed a lame 3D platformer game, where players race with animal avatars in a procedurally generated ravine collecting LSD-inspired candy. The game is a real-time, non-centralized multiplayer game, that uses WebGL, and communicates through a node.js relay server.",
+            "technologies": ["typescript", "node.js", "socket.io", "babylon.js"]
+        }
+    }
+}, {
+    "category": "Corvinus University Budapest",
+    "name": "Business Information Systems, BSc",
+    "from": "2011-08-31",
+    "to": "2013-02-03",
+    "id": 2,
+    "position": "Business Information Systems",
+    "description": "I started a secondary, business/finance focused programme, still in the realm of IT.",
+    "extras": {
+    }
+}, {
+    "category": "Corvinus University Budapest",
+    "name": "Business Information Systems, BSc",
+    "from": "2014-03-09",
+    "to": "2016-03-03",
+    "id": 3,
+    "position": "Business Information Systems",
+    "description": "Continuing the remainder of the programme after a year's hiatus because of work and the MSc.",
+    "extras": {
+        "thesis": {
+            "title": "Portfolio optimization algorithms",
+            "description": "Acquiring, parsing stock data. Training naive strategies to predict stock price movement with back-testing. All components were implemented by me.",
+            "technologies": ["C#"]
+        }
+    }
+}, {
+    "category": "Budapest University of Technology",
+    "name": "Programming Basics (C, C++) course",
+    "from": "2012-08-31",
+    "to": "2014-06-29",
+    "id": 4,
+    "position": "Teaching assistant",
+    "description": "Taught lab courses in C and C++ intro at Budapest University of Technology for freshmen. 3 semesters total.",
+    "extras": {}
+}, {
+    "category": "Corvinus University Budapest",
+    "name": "Javascript course",
+    "from": "2014-08-31",
+    "to": "2015-06-29",
+    "id": 5,
+    "position": "Teacher",
+    "description": "I concieved, organized and taught a Javascript intro course for enthusiasts at Corvinus University. I made the course material, homework, tasks and a presentation/task submission platform. 2 semesters.",
+    "extras": {}
+}, {
+    "category": "Work",
+    "name": "Nokia / NSN",
+    "from": "2012-07-31",
+    "to": "2014-03-14",
+    "id": 6,
+    "position": "Java developer engineer",
+    "description": "I was a part time regular Java developer. I was responsible for developing server side Java, front end, and javascript for analytics and phone control systems. Thorough testing was part of the job.",
+    "extras": {
+        "technologies": ["Java", "Javascript"]
+    }
+}, {
+    "category": "Work",
+    "name": "Morgan Stanley",
+    "from": "2014-07-08",
+    "to": "2014-10-08",
+    "id": 7,
+    "position": "Summer Intern",
+    "description": " I was responsible for developing various parts of a complex trading application’s UI.",
+    "extras": {
+        "technologies": ["C#", "WPF", "Rx"]
+    }
+}, {
+    "category": "Work",
+    "name": "Morgan Stanley",
+    "from": "2015-02-19",
+    "to": "2015-08-29",
+    "id": 8,
+    "position": "Software Developer Intern",
+    "description": "I was developing Scala back end logic for corresponding Javascript front end components for financial software. Including a high-performance 3D surface plot viewer with Three.js.",
+    "extras": {
+        "technologies": ["Scala", "Javascript", "Three.js", "Ext.js"]
+    }
+}, {
+    "category": "Work",
+    "name": "I-Insight ltd.",
+    "from": "2015-08-31",
+    "to": "2016-05-29",
+    "id": 9,
+    "position": "Inisghts Engineer (big data engineer)",
+    "description": "Specializing in solutions that require advanced programming and software engineering knowledge along with analytical and communication skills.",
+    "extras": {
+        "technologies": ["Javascript", "Scala", "Three.js", "Ext.js"]
+    }
+}, {
+    "category": "Work",
+    "name": "Prezi.com",
+    "from": "2016-06-13",
+    "to": "",
+    "id": 10,
+    "position": "Full stack engineer",
+    "description": "Mainly working on collaborative features and document persistence using Scala and Typescript. Our team is responsible for maintaining the document consistency, we build tools and systems to improve editing experience and stability.",
+    "extras": {
+        "technologies": ["Typescript", "Scala", "React", "Haskell"]
+    }
+}]
